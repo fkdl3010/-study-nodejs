3c521f6c596c7ba12cbb2b0e046c3c6f7fc635bf
fkdl3010 <fkdl3919@icloud.com>
2021-03-14 23:13:19 +0900

    node.js

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
- 
+var http = require("http");
+var fs = require("fs");
+var app = http.createServer(function (request, response) {
+  var url = request.url;
+  if (request.url == "/") {
+    url = "/index.html";
+  }
+  if (request.url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  console.log(__dirname + url);
+  response.end(fs.readFileSync(__dirname + url));
 });
 app.listen(3000);
+
+const password = {
+  id: "123",
+  pw: "123",
+  user() {
+    console.log(123);
+    11111111;
+  },
+};
